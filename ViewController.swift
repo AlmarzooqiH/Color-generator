9f1c7ce7c4ecc2bae753d182aa19339a9ab7306b
Hamad Almarzooqi <AlmarzooqiH23@gmail.com>
2021-08-08 20:54:28 +0400

    UI is added.

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Color generator
+//
+//  Created by Hamad Saleh Almarzooqi on 07/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var colorImage1: UIImageView!
+    @IBOutlet weak var colorImage2: UIImageView!
+    @IBOutlet weak var colorImage3: UIImageView!
+    @IBOutlet weak var colorImage4: UIImageView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+    @IBAction func Generate(_ sender: Any) {
+
+    }
+    
+}
+
